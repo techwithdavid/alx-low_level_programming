a30a505b6586a8072f666931e036352bf883beda
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2022-12-11 14:10:46 -0800

    Added the print_comb file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
