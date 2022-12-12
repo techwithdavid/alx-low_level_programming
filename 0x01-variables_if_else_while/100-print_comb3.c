43da3d0b97ddb0c11c4a7e8ac40dd45523c6374c
Fabusuyi Olasunkanmi <117929218+techwithdavid@users.noreply.github.com>
2022-12-12 04:00:44 +0100

    Updated the 100-print_comb3 file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,2 +1,38 @@
-<
-ppp0<<<<<<<<<<
+#include <stdio.h>
+
+/**
+ * main - Entry ponit
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+		d++;
+	}
+
+	putchar('10');
+
+	return (0);
+}
