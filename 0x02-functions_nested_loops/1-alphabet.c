b0ec06b94acfe8e6cc5e07ed5c2bfdadec9608f0
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2022-12-13 18:25:38 -0800

    Updated the file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
 
-	for (i = 'a'; i <= 'z'; i++ )
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
