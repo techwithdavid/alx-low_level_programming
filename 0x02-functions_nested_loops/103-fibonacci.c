642c65dd57a80350f658a114b84f068283619945
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2022-12-13 20:37:36 -0800

    Updated the description

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,6 +3,10 @@
 /**
  * main - Entry point
  *
+ * Description: A program that finds and prints the sum of
+ * The even-valued fibonacci numbers whose values do not
+ * Exceed 4,000,000
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
