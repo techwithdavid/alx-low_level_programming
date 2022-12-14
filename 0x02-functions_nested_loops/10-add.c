bcb10af36ae53fbb5ce0b500a8a616c0dc293031
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2022-12-14 11:15:48 -0800

    Fixed the code

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,11 @@
-#include "main.h"
+#include <stdio.h>
 
+/**
+ * add - adds integers x and y.
+ * @x: First argument
+ * @y: Second argument
+ * Return: Always 0 (Success)
+ */
 int add(int x, int y)
 {
 	return (x + y);
