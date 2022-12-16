b9e45e162679affe6ca19ed371655d676f5ea59e
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2022-12-16 13:35:50 -0800

    Added the file

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int main(void)
+{
+	print_line(9);
+	print_line(2);
+	print_line(-1);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints underscores(line) in n times
+ * @n: number of times underscore should be repeated
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+	_putchar(10);
+}
