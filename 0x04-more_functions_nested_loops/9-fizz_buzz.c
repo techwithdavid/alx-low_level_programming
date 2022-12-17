3c14c6b78a2b9ce6b563ea750e0aa734ba74b019
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2022-12-16 20:42:05 -0800

    Added the file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: The common FizzBuzz algorithm
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i & 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		putchar(' ');
+	}
+
+	putchar(10);
+
+	return (0);
+}
