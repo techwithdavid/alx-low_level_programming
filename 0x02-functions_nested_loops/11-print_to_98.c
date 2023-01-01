de5b7d98b712bc17b80946a8767721f56470b7ed
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-01-01 12:34:40 -0800

    Update: Added the file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%i", n);
+
+			if (n != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n++;
+		}
+	}
+}
