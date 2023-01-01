213f97a89472ef672b3021377379e28b844dba19
Fabusuyi Olasunkanmi <daveed925@gmail.com>
2023-01-01 12:47:22 -0800

    Fix: Fixed the code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,4 +17,23 @@ void print_to_98(int n)
 		}
 		putchar(10);
 	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%i", n);
+			
+			if (n != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n--;
+		}
+		putchar (10);
+	}
+	else
+	{
+		printf("%i\n", n);
+	}
 }
